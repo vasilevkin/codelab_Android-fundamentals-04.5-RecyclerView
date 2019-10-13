14204fbaf2eeebc703b751adbccdf645d6324af5
Sergey Vasilevkin <svasilev@mera.ru>
2019-10-13 17:21:09 +0300

    3.1. Make items respond to clicks

--- RecyclerView/app/src/main/java/com/vasilevkin/recyclerview/WordListAdapter.java
+++ RecyclerView/app/src/main/java/com/vasilevkin/recyclerview/WordListAdapter.java
@@ -41,21 +41,29 @@ public class WordListAdapter extends
         return mWordList.size();
     }
 
-    class WordViewHolder extends RecyclerView.ViewHolder {
+    class WordViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         public final TextView wordItemView;
         final WordListAdapter mAdapter;
-
-//        public WordViewHolder(@NonNull View itemView, TextView wordItemView, WordListAdapter mAdapter) {
-//            super(itemView);
-//            this.wordItemView = wordItemView;
-//            this.mAdapter = mAdapter;
-//        }
 
         public WordViewHolder(View itemView, WordListAdapter adapter) {
             super(itemView);
             wordItemView = itemView.findViewById(R.id.word);
             this.mAdapter = adapter;
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            // Get the position of the item that was clicked.
+            int mPosition = getLayoutPosition();
+            // Use that to access the affected item in mWordList.
+            String element = mWordList.get(mPosition);
+            // Change the word in the mWordList.
+            mWordList.set(mPosition, "Clicked! " + element);
+            // Notify the adapter, that the data has changed so it can
+            // update the RecyclerView to display the data.
+            mAdapter.notifyDataSetChanged();
         }
     }
 
