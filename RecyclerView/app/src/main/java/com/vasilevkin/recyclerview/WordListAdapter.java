be30fa4c9a2c35dcb5b9e6474ed7d9bc39a34509
Sergey Vasilevkin <svasilev@mera.ru>
2019-10-13 16:42:33 +0300

    2.5 Create the ViewHolder for the adapter

--- RecyclerView/app/src/main/java/com/vasilevkin/recyclerview/WordListAdapter.java
+++ RecyclerView/app/src/main/java/com/vasilevkin/recyclerview/WordListAdapter.java
@@ -1,6 +1,8 @@
 package com.vasilevkin.recyclerview;
 
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -22,4 +24,23 @@ public class WordListAdapter extends
     public int getItemCount() {
         return 0;
     }
+
+    class WordViewHolder extends RecyclerView.ViewHolder {
+
+        public final TextView wordItemView;
+        final WordListAdapter mAdapter;
+
+//        public WordViewHolder(@NonNull View itemView, TextView wordItemView, WordListAdapter mAdapter) {
+//            super(itemView);
+//            this.wordItemView = wordItemView;
+//            this.mAdapter = mAdapter;
+//        }
+
+        public WordViewHolder(View itemView, WordListAdapter adapter) {
+            super(itemView);
+            wordItemView = itemView.findViewById(R.id.word);
+            this.mAdapter = adapter;
+        }
+    }
+
 }
